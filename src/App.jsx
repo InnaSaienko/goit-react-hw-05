06df0358c4adc75a6a790188529eef12093559cd
Inna Saienko <inna.saienko.nl@gmail.com>
2025-04-07 19:30:56 +0200

    deleted useEffect, logic realized in useState()

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
 import './App.module.css';
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import ContactList from "./components/ContactList/ContactList.jsx";
 import SearchBox from "./components/SearchBox/SearchBox.jsx";
 import ContactForm from "./components/ContactForm/ContactForm.jsx";
-import {ContactData} from "./constants/ContactData.js";
 import s from "./App.module.css"
 
 export default function App() {
     const [contacts, setContacts] = useState(() =>
         JSON.parse(localStorage.getItem('contacts')) || [])
     const [filter, setFilter] = useState("");
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const storedUsers = JSON.parse(localStorage.getItem('contacts'));
-            if (storedUsers) {
-                setContacts(storedUsers);
-            } else {
-                setContacts(ContactData);
-            }
-        }
-    }, []);
-
-    useEffect(() => {
-        localStorage.setItem('users', JSON.stringify(contacts));
-    }, [contacts]);
 
     const filteredContacts = filter
         ? contacts.filter(contact =>
