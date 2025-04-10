3e99c5f72e9ee14ef4cafcf607548749b8bd94ff
Inna Saienko <inna.saienko.nl@gmail.com>
2025-04-10 15:33:07 +0200

    remove unnecessary console.log

--- src/api.js
+++ src/api.js
@@ -4,24 +4,21 @@ const accessKey = import.meta.env.VITE_UNSPLASH_API_KEY;
 const baseUrl = import.meta.env.VITE_UNSPLASH_URL;
 const pathSearch = "/search/photos";
 
-export const fetchPhotos = async (query, page,  photosPerPage, signal) => {
+export const fetchPhotos = async (query, page, photosPerPage, signal) => {
     console.log("query: ", query);
     const url = new URL(pathSearch, baseUrl);
 
     if (query) {
-            url.searchParams.set("orientation", "landscape");
-            url.searchParams.set("query", query);
+        url.searchParams.set("orientation", "landscape");
+        url.searchParams.set("query", query);
     }
-    url.searchParams.set("per_page",  photosPerPage);
+    url.searchParams.set("per_page", photosPerPage);
     url.searchParams.set("page", page);
-    // console.log(" photosPerPage: ", photosPerPage);
-    // console.log("URL:", url);
-     const response = await axios.get(url, {
-         headers: {
-             Authorization: `Client-ID ${accessKey}`,
-             "Accept-Version": `v1`,
-         }
-     }, {signal});
-    // console.log(response.data.results[0].id);
+    const response = await axios.get(url, {
+        headers: {
+            Authorization: `Client-ID ${accessKey}`,
+            "Accept-Version": `v1`,
+        }
+    }, {signal});
     return response.data.results;
 }
