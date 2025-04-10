24a0a6734be0832cb680ddf40c976120402ad3f3
Inna Saienko <inna.saienko.nl@gmail.com>
2025-04-10 16:35:48 +0200

    change console.log

--- src/api.js
+++ src/api.js
@@ -7,12 +7,12 @@ const pathSearch = "/search/photos";
 export const fetchPhotos = async (query, page, photosPerPage, signal) => {
     console.log("query: ", query);
     const url = new URL(pathSearch, baseUrl);
-
+    console.log("url sub: ", url);
     if (query) {
         // url.searchParams.set("orientation", "landscape");
-        console.log("url sub: ", url);
+        console.log("url if statement : ", url);
         url.searchParams.set("query", query);
-        console.log("url sub after: ", url);
+
     }
     url.searchParams.set("per_page", photosPerPage);
     url.searchParams.set("page", page);
