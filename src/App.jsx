1904cdb2674e9a0bd487dc6cb4656aaa006537cc
Inna Saienko <inna.saienko.nl@gmail.com>
2025-04-16 08:53:13 +0200

    main renders <StrictMode><App /></ StrictMode>.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,71 @@
-import { RouterProvider } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import s from "./App.module.css"
+import {createBrowserRouter, Outlet, RouterProvider} from 'react-router-dom';
+import Navigation from "./components/Navigation/Navigation.jsx";
+import Loader from "./components/Loader/Loader.jsx";
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage.jsx'));
+const MovieDetailsPage = lazy(() => import('./components/MovieDetailsPage/MovieDetailsPage'));
+const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
+const MovieReviews = lazy(() => import('./components/MovieReviews/MovieReviews'));
+const NotFoundPage = lazy(() => import('./components/NotFoundPage/NotFoundPage'));
+
+const Layout = () => {
+    return (
+        <div className={s.wrapper}>
+            <Navigation/>
+            <main className={s.main}>
+                <Suspense fallback={<Loader/>}>
+                    <Outlet/>
+                </ Suspense>
+            </main>
+        </div>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout/>,
+        children: [
+            {
+                index: true,
+                element: <HomePage/>,
+            },
+            {
+                path: '/movies',
+                element: <MoviesPage/>,
+            },
+            {
+                path: 'movies/:id',
+                element: <MovieDetailsPage/>,
+                children: [
+                    {
+                        path: 'cast',
+                        element: <MovieCast/>,
+                    },
+                    {
+                        path: 'reviews',
+                        element: <MovieReviews/>,
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        path: '*',
+        element: <NotFoundPage/>,
+    },
+]);
 
 export default function App() {
-    return <RouterProvider router={router}/>;
+    return (
+        <Suspense fallback={<Loader />}>
+            <RouterProvider router={router} />
+        </Suspense>
+    );
 }
+
+
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,68 +1,9 @@
-import React, {Suspense} from "react";
-import ReactDOM from "react-dom/client";
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
-import './index.css';
-import Loader from "./components/Loader/Loader.jsx";
-import Navigation from "./components/Navigation/Navigation.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const HomePage = React.lazy(() => import('./pages/HomePage/HomePage.jsx'));
-const MoviesPage = React.lazy(() => import('./pages/MoviesPage/MoviesPage.jsx'));
-const MovieDetailsPage = React.lazy(() => import('./components/MovieDetailsPage/MovieDetailsPage'));
-const MovieCast = React.lazy(() => import('./components/MovieCast/MovieCast'));
-const MovieReviews = React.lazy(() => import('./components/MovieReviews/MovieReviews'));
-const NotFoundPage = React.lazy(() => import('./components/NotFoundPage/NotFoundPage'));
-
-
-const Layout = () => {
-    return (
-        <div className="wrapper">
-            <Navigation/>
-            <main className="main">
-                <Suspense fallback={<Loader/>}>
-                    <Outlet/>
-                </ Suspense>
-            </main>
-        </div>
-    );
-};
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout/>,
-        children: [
-            {
-                index: true,
-                element: <HomePage/>,
-            },
-            {
-                path: '/movies',
-                element: <MoviesPage/>,
-            },
-            {
-                path: 'movies/:id',
-                element: <MovieDetailsPage/>,
-                children: [
-                    {
-                        path: 'cast',
-                        element: <MovieCast/>,
-                    },
-                    {
-                        path: 'reviews',
-                        element: <MovieReviews/>,
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        path: '*',
-        element: <NotFoundPage/>,
-    },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <RouterProvider router={router} />
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
