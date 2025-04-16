ac5fd42d99fb7cffdf5fb617acd08218fa0a0af4
Inna Saienko <inna.saienko.nl@gmail.com>
2025-04-16 09:45:33 +0200

    add transmit location when navigating to another page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import s from "./MovieList.module.css";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import {getFullImageUrl} from "../../utils/utils.js";
 
 const MovieList = ({movies}) => {
-
+    const location = useLocation();
     return (
         <ul className={s.gridList}>
             {movies.map((movie) => (
                 <li key={movie.id} className={s.g_item}>
-                        <Link to={`/movies/${movie.id}`}>
+                        <Link to={`/movies/${movie.id}`} state={{ from: location }}>
                             <h2 className={s.title}>{movie.title}</h2>
                             <div className={s.item_poster}>
                             <img src={getFullImageUrl(movie.backdrop_path)}
